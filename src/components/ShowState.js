ac7a3f1e24dee872e2b7fa12c5c1bb97428a47de
wayne <wayneborong@gmail.com>
2019-08-07 00:11:10 +1000

    update

--- src/components/ShowState.js
+++ src/components/ShowState.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import $ from 'jquery'
-import { request } from 'https';
+import SWApi from '../constants/Apis'
 
 // HOC test
 //async function test
@@ -11,8 +11,15 @@ async function hello(){
     return 'this is a promise'
 }
 const h = hello();
+// $.get(SWApi).then(res =>console.log('SWAPI people:'+res))
 
-h.then(res =>console.log(res))
+const promise = async ()=>{
+    const step1 = $.get(SWApi);
+    const Luke = await step1;
+    console.log(Luke);
+}
+const luke  = promise();
+// console.log(promise())
 
 export default function ShowState() {
 
---
+++ src/constants/Apis.js
@@ -0,0 +1,3 @@
+const SWApi ='https://swapi.co/api/people/1/';
+
+export default SWApi
